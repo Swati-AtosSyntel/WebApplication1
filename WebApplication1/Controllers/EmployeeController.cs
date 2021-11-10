bbddb904426df37cc459e79c8c5d36b6b59be9c9
nbhir <nbhir@DESKTOP-RKMH2OS>
2021-11-10 16:50:55 +0530

    Add project files.

---
+++ WebApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            Employee e = new Employee() { empid = 123, ename = "Swati", location = "Pune" };
+            List<Employee> employees = new List<Employee>()
+            {
+                new Employee(){empid=111,ename="Aniket",location="Pune"},
+                new Employee(){empid=112,ename="Nishant",location="Mumbai"},
+                new Employee(){empid=113,ename="Omkar",location="Chennai"}
+            };
+            ViewData["employee"] = e;
+            ViewBag.emp = e;
+            return View(employees);
+        }
+        public string AboutUs()
+        {
+            return "This is about us page";
+        }
+    }
+}
---
+++ WebApplication1/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Employee
+    {
+        public int empid { get; set; }
+        public string ename { get; set; }
+        public string location { get; set; }
+    }
+}
